8e356654f28bc235016116d7806bcb0813b5ea37
Matt Zamora <mzamora2899@gmail.com>
2022-11-08 18:06:11 -0700

    cleanup

--- src/layout/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-interface IProps {
-    children: JSX.Element | JSX.Element[]
-}
-const Main: FC<IProps> = ({ children }) => {
-    return <div className="w-full h-screen">{children}</div>
-}
-export default Main
