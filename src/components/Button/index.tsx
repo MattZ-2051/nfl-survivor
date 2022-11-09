de735e763610c7faedae2ccdc99b2706b7b675c9
Matt Zamora <mzamora2899@gmail.com>
2022-11-08 17:34:43 -0700

    add header component and navigations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 type ButtonType = 'primary' | 'outlined'
-
 interface IProps {
-    children?: JSX.Element
     type: ButtonType
-    onClick?: () => void
+    onClick?: () => void | undefined
     classes?: string
     label: string
+    redirectTo?: string
 }
 
 const buttonTypeMap: Record<ButtonType, Record<string, string>> = {
@@ -18,13 +18,26 @@ const buttonTypeMap: Record<ButtonType, Record<string, string>> = {
         class: 'inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out',
     },
 }
-const Button: FC<IProps> = ({ label, children, type, onClick, classes }) => {
+const Button: FC<IProps> = ({ label, type, onClick, classes, redirectTo }) => {
+    if (redirectTo) {
+        return (
+            <Link
+                type="button"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+                className={buttonTypeMap[type].class}
+                to={redirectTo}
+            >
+                {label}
+            </Link>
+        )
+    }
     return (
         <button
             type="button"
             data-mdb-ripple="true"
             data-mdb-ripple-color="light"
-            className={`${(buttonTypeMap[type].class, classes)}`}
+            className={buttonTypeMap[type].class}
             onClick={onClick}
         >
             {label}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,23 @@ import {
     createRoutesFromElements,
     Route,
 } from 'react-router-dom'
+
 import ErrorPage from '@pages/ErrorPage'
 import Home from '@pages/Home'
+import Groups from '@pages/Groups'
 
-const router = createBrowserRouter(
+export const routes = {
+    home: '/',
+    groups: '/groups',
+}
+
+export const browserRouter = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+        <>
+            <Route errorElement={<ErrorPage />}>
+                <Route path={routes.home} element={<Home />} />
+                <Route path={routes.groups} element={<Groups />} />
+            </Route>
+        </>
     )
 )
-export default router
