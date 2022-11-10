d0e51cd23ba831821949da764ffa4708fa2139fd
Matt Zamora <mzamora2899@gmail.com>
2022-11-10 14:56:18 -0700

    backend and db connection setup

---
+++ nfl-survivor-backend/api/api_config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import dotenv_values
+
+config = {
+  "api_config": {
+    "dev": {**dotenv_values("../.env.development")},
+    "prod": {**dotenv_values("../.env.production")},
+    **os.environ
+  }
+  # **os.environ
+}
