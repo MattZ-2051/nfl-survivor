b929d90f69aadb602715c7640a1885548365bca7
Matt Zamora <mzamora2899@gmail.com>
2022-11-14 16:01:05 -0700

    modify relationships and add test rows

--- nfl-survivor-backend/api/survivor/models.py
+++ nfl-survivor-backend/api/survivor/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50, null=False)
-    password = models.CharField(null=False, max_length=200)
-
-
-class Result(models.Model):
-    winner = models.OneToOneField(User, on_delete=models.CASCADE)
-
 
 class Game(models.Model):
     code = models.CharField(null=False, max_length=200)
     name = models.CharField(max_length=20, null=False)
-    players = models.ManyToManyField(User)
-    result = models.OneToOneField(Result, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, null=False)
+    password = models.CharField(null=False, max_length=200)
+    games = models.ForeignKey(Game, null=True, on_delete=models.CASCADE)
+
+
+
 
 
 # matt_user = User(username="matt", password="password")
-# game_1 = Game(code="1234", name="test", winner=None, users=matt_user)
+# test_user = User(username="test", password="password")
+# test_user.save()
+# game = Game(code="123", name="test")
+# game.save()
