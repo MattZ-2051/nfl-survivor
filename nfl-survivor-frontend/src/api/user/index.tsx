c7286e37f435701f7f3494fe346a98d382d9b78c
Matt Zamora <mzamora2899@gmail.com>
2022-11-22 19:21:50 -0700

    update requirements and env examples

--- nfl-survivor-frontend/src/api/user/index.tsx
+++ nfl-survivor-frontend/src/api/user/index.tsx
@@ -1,17 +1,16 @@
 import { createEffect } from 'effector'
-import type { AuthTokens } from '@types/user'
+import type { NewUser, User } from '@types/user'
 import { get, post } from '../index'
 
-export const signup = createEffect<
-    { username: string; password: string; rePassword: string },
-    AuthTokens
->(async ({ username, password, rePassword }) => {
-    return await post('/api/auth/signup', {
-        username,
-        password,
-        re_password: rePassword,
-    })
-})
+export const signup = createEffect<NewUser, User>(
+    async ({ username, password, rePassword }) => {
+        return await post('/api/auth/signup', {
+            username,
+            password,
+            re_password: rePassword,
+        })
+    }
+)
 
 export const sessionDeleteFx = createEffect<void, null>(() => {})
 
