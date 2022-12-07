3dd210f0dc3ca075a534e24c36a9dba32809810d
Matt Zamora <mzamora2899@gmail.com>
2022-12-06 18:10:00 -0700

    modify dockerfiles and composition

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteAliases } from 'vite-aliases'
 
+const PORT = 3000
 export default defineConfig(() => {
     return {
         // vite config
         plugins: [react(), ViteAliases()],
-        server: { port: 3000 },
+        server: {
+            host: '0.0.0.0',
+            proxy: {
+                '/api': {
+                    target: `http://localhost:${PORT}`,
+                    changeOrigin: true,
+                },
+            },
+        },
+        build: {
+            outDir: 'dist/frontend',
+        },
     }
 })
